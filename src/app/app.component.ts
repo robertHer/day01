f38f18c2df067f61217d25cbd2617fa307768a17
unknown <totolero769@gmail.com>
2018-05-30 10:19:52 -0700

    examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit { 
+  ngOnInit {
+    this.str = 'this is a string';
+   this.test();
+   this.numberArrayInput([]);
+   this.numberArrayInput([5]);
+  }
   title = 'app';
+
+  str: string =  'this is a string';
+
+  // ngOnInit {
+  //   
+  // }
+
+  test(){
+    let name: string = 'Test User';
+    let age:number = 30;
+    let text: string = `My name is ${name} and i am ${age} years old`;
+  }
+
+  arrayInput(x:number[]){
+    console.log(x);
+  }
+  anyArrayInput(x:Array<any>){
+    console.log(x);
+  }
 }
